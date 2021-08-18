efb143ccd669a0558ed9c6c07ddf78809ab88767
Mario <mario362880@gmail.com>
2021-08-18 13:27:28 +0200

    Add swipe up for video

--- lib/players/components/youtubePlayer/collapsedPanel.dart
+++ lib/players/components/youtubePlayer/collapsedPanel.dart
@@ -22,125 +22,171 @@ class VideoPageCollapsed extends StatelessWidget {
       decoration: BoxDecoration(
           color: Theme.of(context).cardColor,
           borderRadius: BorderRadius.circular(10)),
-      child: Row(
+      child: Column(
         children: [
-          Expanded(
+          Container(
+            margin: EdgeInsets.only(top: 1.2),
             child: Row(
-              mainAxisSize: MainAxisSize.min,
               children: [
-                Container(
-                  height: 50,
-                  width: 50,
-                  margin: EdgeInsets.only(left: 8),
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: pageProvider.infoItem is StreamInfoItem
-                          ? FadeInImage(
-                              fadeInDuration: Duration(milliseconds: 400),
-                              placeholder: MemoryImage(kTransparentImage),
-                              image: NetworkImage(thumbnailUrl),
-                              fit: BoxFit.cover,
-                            )
-                          : Container(
-                              decoration: BoxDecoration(
-                                  color:
-                                      Theme.of(context).scaffoldBackgroundColor,
-                                  borderRadius: BorderRadius.circular(50)),
-                              child: Icon(MdiIcons.playlistMusic,
-                                  color: Theme.of(context).iconTheme.color),
-                            )),
+                Icon(
+                  EvaIcons.arrowUp,
+                  size: 11,
                 ),
-                Expanded(
-                  child: Container(
-                    margin: EdgeInsets.only(left: 8),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        MarqueeWidget(
-                          animationDuration: Duration(seconds: 8),
-                          backDuration: Duration(seconds: 3),
-                          pauseDuration: Duration(seconds: 2),
-                          direction: Axis.horizontal,
-                          child: Text(
-                            "$title",
-                            style:
-                                TextStyle(fontFamily: 'YTSans', fontSize: 16),
-                            maxLines: 1,
-                            overflow: TextOverflow.fade,
-                            softWrap: false,
-                          ),
-                        ),
-                        if (author != "") SizedBox(height: 2),
-                        if (author != "")
-                          Text(
-                            "$author",
-                            style: TextStyle(
-                                fontFamily: 'YTSans',
-                                fontSize: 11,
-                                color: Theme.of(context)
-                                    .textTheme
-                                    .bodyText1
-                                    .color
-                                    .withOpacity(0.6)),
-                            overflow: TextOverflow.fade,
-                            softWrap: false,
-                            maxLines: 1,
-                          )
-                      ],
-                    ),
-                  ),
-                )
+                Icon(
+                  EvaIcons.arrowUp,
+                  size: 11,
+                ),
+                Icon(
+                  EvaIcons.arrowUp,
+                  size: 11,
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Text("SWIPE UP FOR MORE",textAlign: TextAlign.center,style: TextStyle(fontSize: 13),),
+                SizedBox(
+                  width: 15,
+                ),
+                Icon(
+                  EvaIcons.arrowUp,
+                  size: 11,
+                ),
+                Icon(
+                  EvaIcons.arrowUp,
+                  size: 11,
+                ),
+                Icon(
+                  EvaIcons.arrowUp,
+                  size: 11,
+                ),
               ],
+              mainAxisAlignment: MainAxisAlignment.center,
             ),
           ),
-          // Play/Pause
-          SizedBox(width: 8),
-          Container(
-            color: Colors.transparent,
-            child: IconButton(
-                icon: (AudioStreamPlayer.isAudioPlayer && AudioStreamPlayer.isPlaying) || (!AudioStreamPlayer.isAudioPlayer && AudioStreamPlayer.isVideoPlaying)
-                    ? Icon(MdiIcons.pause, size: 22)
-                    : Icon(MdiIcons.play, size: 22),
-                onPressed: () {
-                  if (AudioStreamPlayer.isAudioPlayer) {
-                    if (AudioStreamPlayer.isPlaying) {
-                      AudioStreamPlayer.pause();
-                      pageProvider.setState();
-                      AudioStreamPlayer.isPlaying = false;
-                    }else{
-                      AudioStreamPlayer.play();
-                      pageProvider.setState();
-                      AudioStreamPlayer.isPlaying = true;
-                    }
-                  } else {
-                    if(AudioStreamPlayer.isVideoPlaying){
-                      AudioStreamPlayer.videoPlayerController.pause();
-                      AudioStreamPlayer.isVideoPlaying = false;
-                      pageProvider.setState();
-                    }else{
-                      AudioStreamPlayer.videoPlayerController.play();
-                      AudioStreamPlayer.isVideoPlaying = true;
-                      pageProvider.setState();
-                    }
-                  }
-                }),
-          ),
-          InkWell(
-            onTap: () {
-              AudioStreamPlayer.stop();
-              AudioStreamPlayer.videoPlayerController.pause();
-              AudioStreamPlayer.isAudioPlayer = true;
-              pageProvider.closeVideoPanel();
-            },
-            child: Ink(
+          Row(
+            children: [
+              Expanded(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      height: 45,
+                      width: 45,
+                      margin: EdgeInsets.only(left: 8),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: pageProvider.infoItem is StreamInfoItem
+                              ? FadeInImage(
+                                  fadeInDuration: Duration(milliseconds: 400),
+                                  placeholder: MemoryImage(kTransparentImage),
+                                  image: NetworkImage(thumbnailUrl),
+                                  fit: BoxFit.cover,
+                                )
+                              : Container(
+                                  decoration: BoxDecoration(
+                                      color: Theme.of(context)
+                                          .scaffoldBackgroundColor,
+                                      borderRadius: BorderRadius.circular(50)),
+                                  child: Icon(MdiIcons.playlistMusic,
+                                      color: Theme.of(context).iconTheme.color),
+                                )),
+                    ),
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.only(left: 8),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            MarqueeWidget(
+                              animationDuration: Duration(seconds: 8),
+                              backDuration: Duration(seconds: 3),
+                              pauseDuration: Duration(seconds: 2),
+                              direction: Axis.horizontal,
+                              child: Text(
+                                "$title",
+                                style: TextStyle(
+                                    fontFamily: 'YTSans', fontSize: 16),
+                                maxLines: 1,
+                                overflow: TextOverflow.fade,
+                                softWrap: false,
+                              ),
+                            ),
+                            if (author != "") SizedBox(height: 2),
+                            if (author != "")
+                              Text(
+                                "$author",
+                                style: TextStyle(
+                                    fontFamily: 'YTSans',
+                                    fontSize: 11,
+                                    color: Theme.of(context)
+                                        .textTheme
+                                        .bodyText1
+                                        .color
+                                        .withOpacity(0.6)),
+                                overflow: TextOverflow.fade,
+                                softWrap: false,
+                                maxLines: 1,
+                              )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              // Play/Pause
+              SizedBox(width: 8),
+              Container(
                 color: Colors.transparent,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Icon(EvaIcons.closeOutline),
-                )),
+                child: IconButton(
+                    icon: (AudioStreamPlayer.isAudioPlayer &&
+                                AudioStreamPlayer.isPlaying) ||
+                            (!AudioStreamPlayer.isAudioPlayer &&
+                                AudioStreamPlayer.isVideoPlaying)
+                        ? Icon(MdiIcons.pause, size: 22)
+                        : Icon(MdiIcons.play, size: 22),
+                    onPressed: () {
+                      if (AudioStreamPlayer.isAudioPlayer) {
+                        if (AudioStreamPlayer.isPlaying) {
+                          AudioStreamPlayer.pause();
+                          pageProvider.setState();
+                          AudioStreamPlayer.isPlaying = false;
+                        } else {
+                          AudioStreamPlayer.play();
+                          pageProvider.setState();
+                          AudioStreamPlayer.isPlaying = true;
+                        }
+                      } else {
+                        if (AudioStreamPlayer.isVideoPlaying) {
+                          AudioStreamPlayer.videoPlayerController.pause();
+                          AudioStreamPlayer.isVideoPlaying = false;
+                          pageProvider.setState();
+                        } else {
+                          AudioStreamPlayer.videoPlayerController.play();
+                          AudioStreamPlayer.isVideoPlaying = true;
+                          pageProvider.setState();
+                        }
+                      }
+                    }),
+              ),
+              InkWell(
+                onTap: () {
+                  AudioStreamPlayer.stop();
+                  AudioStreamPlayer.videoPlayerController.pause();
+                  AudioStreamPlayer.isAudioPlayer = true;
+                  pageProvider.closeVideoPanel();
+                },
+                child: Ink(
+                    color: Colors.transparent,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(EvaIcons.closeOutline),
+                    )),
+              ),
+              SizedBox(width: 16)
+            ],
           ),
-          SizedBox(width: 16)
         ],
       ),
     );
